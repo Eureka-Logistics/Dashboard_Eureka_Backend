722200fe2a40fcae6733f7087318c852d1f913d0
bejo123 <vicoramdhani@gmail.com>
2025-03-10 19:49:29 +0700

    ubah konek db

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://Eudabord:Salam_Super!Victory@34.101.78.144:27017/Eureka_Group');
+    await mongoose.connect('mongodb://superViktoriAdmin:superViktoriPassword@34.101.254.58:27117/?authSource=admin');
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB connection error:", error);
