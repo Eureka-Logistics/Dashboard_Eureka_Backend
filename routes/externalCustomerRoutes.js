6b6b111683cb0e8560525f06bc5db13fc3193985
bejo123 <vicoramdhani@gmail.com>
2025-11-12 15:40:54 +0700

    nambahin ednpoint db sandi

--- routes/externalCustomerRoutes.js
+++ routes/externalCustomerRoutes.js
@@ -4,5 +4,7 @@ const controller = require('../controllers/externalCustomerController');
 
 router.get('/', controller.getAllCustomers);
 router.get('/get/:id', controller.getCustomerById); // id refers to id_user
+// Alias to support /external-customers/:id as requested
+router.get('/:id', controller.getCustomerById);
 
 module.exports = router;
